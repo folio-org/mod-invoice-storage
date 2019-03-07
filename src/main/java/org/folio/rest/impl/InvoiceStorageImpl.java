99b0f4dc5fb07a60ed30559b9ca076f3e7ef0166
Aliaksandr Pautau <aliaksandr_pautau@epam.com>
2019-03-07 17:41:41 +0300

    MODINVOSTO-2 Define APIs
    - permissions names fixed
    - fixed invoice number path in raml
    - return 501 code for not implemented endpoints
    - use test suite
    - pom updated

--- src/main/java/org/folio/rest/impl/InvoiceStorageImpl.java
+++ src/main/java/org/folio/rest/impl/InvoiceStorageImpl.java
@@ -20,71 +20,71 @@ public class InvoiceStorageImpl implements InvoiceStorage {
   @Validate
   @Override
   public void getInvoiceStorageInvoices(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoicesResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void postInvoiceStorageInvoices(String lang, Invoice entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PostInvoiceStorageInvoicesResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void getInvoiceStorageInvoicesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoicesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void deleteInvoiceStorageInvoicesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(DeleteInvoiceStorageInvoicesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void putInvoiceStorageInvoicesById(String id, String lang, Invoice entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PutInvoiceStorageInvoicesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void getInvoiceStorageInvoiceLines(int offset, int limit, String query, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoiceLinesResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void postInvoiceStorageInvoiceLines(String lang, InvoiceLine entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PostInvoiceStorageInvoiceLinesResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void putInvoiceStorageInvoiceLinesById(String id, String lang, InvoiceLine entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(PutInvoiceStorageInvoiceLinesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void getInvoiceStorageInvoiceLinesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoiceLinesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Validate
   @Override
   public void deleteInvoiceStorageInvoiceLinesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(DeleteInvoiceStorageInvoiceLinesByIdResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
 
   @Override
   public void getInvoiceStorageInvoiceNumber(String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoiceNumberResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 
   @Override
   public void getInvoiceStorageInvoiceLineNumber(String invoiceId, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    asyncResultHandler.handle(succeededFuture(GetInvoiceStorageInvoiceLineNumberResponse.respond500WithTextPlain(NOT_SUPPORTED)));
+    asyncResultHandler.handle(succeededFuture(Response.status(501).build()));
   }
 }
---
+++ src/test/java/org/folio/rest/impl/StorageTestSuite.java
@@ -0,0 +1,154 @@
+package org.folio.rest.impl;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.response.ValidatableResponse;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.folio.rest.RestVerticle;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.client.test.HttpClientMock2;
+import org.folio.rest.tools.utils.NetworkUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static io.restassured.RestAssured.given;
+import static org.folio.rest.impl.TestBase.TENANT_HEADER;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+  CrudTest.class
+})
+
+public class StorageTestSuite {
+  private static final Logger logger = LoggerFactory.getLogger(StorageTestSuite.class);
+
+  private static Vertx vertx;
+  private static int port = NetworkUtils.nextFreePort();
+  private static final Header URL_TO_HEADER = new Header("X-Okapi-Url-to","http://localhost:"+port);
+  private static final Header USER_ID_HEADER = new Header("X-Okapi-User-id", "28d0fb04-d137-11e8-a8d5-f2801f1b9fd1");
+  private static final String TENANT_ENDPOINT = "/_/tenant";
+
+  private StorageTestSuite() {}
+
+  public static URL storageUrl(String path) throws MalformedURLException {
+    return new URL("http", "localhost", port, path);
+  }
+
+  public static Vertx getVertx() {
+    return vertx;
+  }
+
+  @BeforeClass
+  public static void before() throws IOException, InterruptedException, ExecutionException, TimeoutException {
+
+    // tests expect English error messages only, no Danish/German/...
+    Locale.setDefault(Locale.US);
+
+
+    vertx = Vertx.vertx();
+
+    logger.info("Start embedded database");
+    PostgresClient.setIsEmbedded(true);
+    PostgresClient.getInstance(vertx).startEmbeddedPostgres();
+
+    DeploymentOptions options = new DeploymentOptions();
+
+    options.setConfig(new JsonObject().put("http.port", port).put(HttpClientMock2.MOCK_MODE, "true"));
+    options.setWorker(true);
+
+    startVerticle(options);
+
+    prepareTenant();
+  }
+
+  @AfterClass
+  public static void after() throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+    logger.info("Delete tenant");
+    deleteTenant();
+
+    CompletableFuture<String> undeploymentComplete = new CompletableFuture<>();
+
+    vertx.close(res -> {
+      if(res.succeeded()) {
+        undeploymentComplete.complete(null);
+      }
+      else {
+        undeploymentComplete.completeExceptionally(res.cause());
+      }
+    });
+
+    undeploymentComplete.get(60, TimeUnit.SECONDS);
+    logger.info("Stop database");
+    PostgresClient.stopEmbeddedPostgres();
+  }
+
+  private static void startVerticle(DeploymentOptions options)
+    throws InterruptedException, ExecutionException, TimeoutException {
+
+    logger.info("Start verticle");
+
+    CompletableFuture<String> deploymentComplete = new CompletableFuture<>();
+
+    vertx.deployVerticle(RestVerticle.class.getName(), options, res -> {
+      if(res.succeeded()) {
+        deploymentComplete.complete(res.result());
+      }
+      else {
+        deploymentComplete.completeExceptionally(res.cause());
+      }
+    });
+
+    deploymentComplete.get(60, TimeUnit.SECONDS);
+  }
+
+  private static void prepareTenant() throws MalformedURLException {
+    JsonObject jsonBody = prepareTenantBody();
+    postToTenant(jsonBody).statusCode(201);
+  }
+
+  private static JsonObject prepareTenantBody() {
+    JsonObject jsonBody = new JsonObject();
+    jsonBody.put("module_to", "mod-orders-storage-1.0.0");
+    return jsonBody;
+  }
+
+  private static ValidatableResponse postToTenant(JsonObject jsonBody) throws MalformedURLException {
+    return given()
+      .header(TENANT_HEADER)
+      .header(URL_TO_HEADER)
+      .header(USER_ID_HEADER)
+      .contentType(ContentType.JSON)
+      .body(jsonBody.encodePrettily())
+        .post(storageUrl(TENANT_ENDPOINT))
+          .then();
+  }
+
+  private static void deleteTenant()
+    throws MalformedURLException {
+    given()
+      .header(TENANT_HEADER)
+      .contentType(ContentType.JSON)
+      .delete(storageUrl(TENANT_ENDPOINT))
+        .then()
+          .statusCode(204);
+  }
+
+
+}
