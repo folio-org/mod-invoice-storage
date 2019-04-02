0c30d8b6bf6320776f85f29ed9fe7053ea4e6fbf
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2019-04-02 13:11:53 +0300

    MODINVOSTO-6 # Improving unit test for including error scenario

--- src/test/java/org/folio/rest/impl/InvoiceNumberTest.java
+++ src/test/java/org/folio/rest/impl/InvoiceNumberTest.java
@@ -1,7 +1,12 @@
 package org.folio.rest.impl;
 
+import io.restassured.http.ContentType;
+import io.vertx.core.Vertx;
+import io.vertx.ext.sql.UpdateResult;
 import io.vertx.ext.unit.junit.Repeat;
 import io.vertx.ext.unit.junit.RepeatRule;
+import org.folio.HttpStatus;
+import org.folio.rest.persist.PostgresClient;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -10,7 +15,12 @@ import org.junit.Test;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.folio.rest.impl.StorageTestSuite.storageUrl;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -22,6 +32,7 @@ public class InvoiceNumberTest extends TestBase {
 
   private static final String SEQUENCE_NUMBER = "sequenceNumber";
   private static final String INVOICE_NUMBER_ENDPOINT = "/invoice-storage/invoice-number";
+  private static final String DROP_SEQUENCE_QUERY = "DROP SEQUENCE diku_mod_invoice_storage.invoice_number";
 
   @BeforeClass
   public static void setUp() {
@@ -35,17 +46,45 @@ public class InvoiceNumberTest extends TestBase {
   }
 
   @AfterClass
-  public static void tearDown() {
+  public static void tearDown() throws Exception {
     assertThat(invoiceNumberList.get(1) - invoiceNumberList.get(0), equalTo(1L));
     assertThat(invoiceNumberList.get(2) - invoiceNumberList.get(0), equalTo(2L));
+
+    // Negative scenario - retrieving number from non-existed sequence
+    dropSequenceInDb();
+    testProcessingErrorReply();
   }
 
   private long getNumberAsLong() throws MalformedURLException {
     return new Long(getData(INVOICE_NUMBER_ENDPOINT)
       .then()
-        .statusCode(200)
+        .statusCode(HttpStatus.HTTP_OK.toInt())
         .extract()
           .response()
             .path(SEQUENCE_NUMBER));
+  }
+
+  private static void testProcessingErrorReply() throws MalformedURLException {
+    given()
+      .header(TENANT_HEADER)
+      .contentType(ContentType.JSON)
+        .get(storageUrl(INVOICE_NUMBER_ENDPOINT))
+          .then()
+            .statusCode(HttpStatus.HTTP_BAD_REQUEST.toInt())
+            .contentType(TEXT_PLAIN)
+            .extract()
+              .response();
+  }
+
+  private static void dropSequenceInDb() throws Exception {
+    CompletableFuture<UpdateResult> future = new CompletableFuture<>();
+      PostgresClient.getInstance(Vertx.vertx()).execute(DROP_SEQUENCE_QUERY, result -> {
+        if(result.failed()) {
+          future.completeExceptionally(result.cause());
+        } else {
+          future.complete(result.result());
+        }
+      });
+      future.get(10, TimeUnit.SECONDS);
   }
 }
