9e6461de64cdf2433161f1d73116d9a144196da7
kvuppala <kvuppala@ebsco.com>
2019-05-07 16:38:35 -0400

    adding voucher line implementation

--- src/test/java/org/folio/rest/utils/TestEntities.java
+++ src/test/java/org/folio/rest/utils/TestEntities.java
@@ -4,11 +4,13 @@ package org.folio.rest.utils;
 import org.folio.rest.jaxrs.model.Invoice;
 import org.folio.rest.jaxrs.model.InvoiceLine;
 import org.folio.rest.jaxrs.model.Voucher;
+import org.folio.rest.jaxrs.model.VoucherLine;
 
 public enum TestEntities {
   INVOICE("/invoice-storage/invoices", Invoice.class, "invoice.sample", "note", "Updated note for invoice", 0),
   INVOICE_LINES("/invoice-storage/invoice-lines", InvoiceLine.class, "invoice_line.sample", "quantity", 5, 0),
-  VOUCHER("/voucher-storage/vouchers", Voucher.class, "voucher.sample", "batchNumber", "202", 0);
+  VOUCHER("/voucher-storage/vouchers", Voucher.class, "voucher.sample", "batchNumber", "202", 0),
+  VOUCHER_LINES("/voucher-storage/voucher-lines", VoucherLine.class, "voucher_line.sample", "batchNumber", "202", 0);
 
   TestEntities(String endpoint, Class<?> clazz, String sampleFileName, String updatedFieldName, Object updatedFieldValue, int initialQuantity) {
     this.endpoint = endpoint;
