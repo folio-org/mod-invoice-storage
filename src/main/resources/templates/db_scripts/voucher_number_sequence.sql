5e411bf6f729fdebde8c8b847eec13fd51412001
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2019-05-16 13:20:26 +0300

    MODINVOSTO-12 # Unique index improvement

--- src/main/resources/templates/db_scripts/voucher_number_sequence.sql
+++ src/main/resources/templates/db_scripts/voucher_number_sequence.sql
@@ -1,3 +1,2 @@
 CREATE SEQUENCE IF NOT EXISTS ${myuniversity}_${mymodule}.voucher_number MINVALUE 0 NO MAXVALUE CACHE 1 NO CYCLE;
 GRANT USAGE ON SEQUENCE ${myuniversity}_${mymodule}.voucher_number TO ${myuniversity}_${mymodule};
-CREATE UNIQUE INDEX IF NOT EXISTS invoices_voucher_number_unique_idx ON ${myuniversity}_${mymodule}.invoices ((jsonb->>'voucherNumber'));
