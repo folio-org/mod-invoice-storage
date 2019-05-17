28b914f92ead41bd760bc46a1085b7bda5f2523c
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2019-05-17 17:16:39 +0300

    MODINVOSTO-13 # Implement POST /voucher-storage/voucher-number/start/<value>

--- src/main/resources/templates/db_scripts/voucher_number_sequence.sql
+++ src/main/resources/templates/db_scripts/voucher_number_sequence.sql
@@ -1,2 +1,3 @@
 CREATE SEQUENCE IF NOT EXISTS ${myuniversity}_${mymodule}.voucher_number MINVALUE 0 NO MAXVALUE CACHE 1 NO CYCLE;
 GRANT USAGE ON SEQUENCE ${myuniversity}_${mymodule}.voucher_number TO ${myuniversity}_${mymodule};
+ALTER SEQUENCE ${myuniversity}_${mymodule}.voucher_number OWNER TO ${myuniversity}_${mymodule};
--- src/test/java/org/folio/rest/impl/VoucherNumberTest.java
+++ src/test/java/org/folio/rest/impl/VoucherNumberTest.java
@@ -3,13 +3,8 @@ package org.folio.rest.impl;
 import io.restassured.http.ContentType;
 import io.vertx.core.Vertx;
 import io.vertx.ext.sql.UpdateResult;
-import io.vertx.ext.unit.junit.Repeat;
-import io.vertx.ext.unit.junit.RepeatRule;
 import org.folio.HttpStatus;
 import org.folio.rest.persist.PostgresClient;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
@@ -22,31 +17,26 @@ import static io.restassured.RestAssured.given;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.folio.rest.impl.StorageTestSuite.storageUrl;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class VoucherNumberTest extends TestBase {
 
-  @Rule
-  public RepeatRule rule = new RepeatRule();
-  private static List<Long> voucherNumberList;
+  private static List<Long> voucherNumberList = new ArrayList<>();
 
   private static final String SEQUENCE_NUMBER = "sequenceNumber";
   private static final String VOUCHER_NUMBER_ENDPOINT = "/voucher-storage/voucher-number";
+  private static final String VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT = "/voucher-storage/voucher-number/start/";
   private static final String DROP_SEQUENCE_QUERY = "DROP SEQUENCE diku_mod_invoice_storage.voucher_number";
-
-  @BeforeClass
-  public static void setUp() {
-    voucherNumberList  = new ArrayList<>();
-  }
+  private static final int NUM_OF_REQUESTS = 3;
 
   @Test
-  @Repeat(3)
-  public void testGetVoucherNumber() throws MalformedURLException {
-    voucherNumberList.add(getNumberAsLong());
-  }
+  public void testGetVoucherNumber() throws Exception {
 
-  @AfterClass
-  public static void tearDown() throws Exception {
+    // Retrieve voucher numbers
+    for(int i = 0; i < NUM_OF_REQUESTS; i++) {
+      voucherNumberList.add(getSequenceNumber());
+    }
 
     // Verify expected start value
     assertThat(voucherNumberList.get(0), equalTo(0L));
@@ -56,12 +46,21 @@ public class VoucherNumberTest extends TestBase {
       assertThat(voucherNumberList.get(i) - voucherNumberList.get(0), equalTo((long) i));
     }
 
-    // Negative scenario - retrieving number from non-existed sequence
+    // Verify start value changing
+    long start = 11111111L;
+    changeStartValue(start);
+    assertThat(getSequenceNumber(), is(start));
+
+    // Negative scenario - changing start value with bad request
+    changeStartValueWithBadRequest();
+
+    // Negative scenario - retrieving number and changing start value from non-existed (deleted) sequence
     dropSequenceInDb();
-    testProcessingErrorReply();
+    getSequenceNumberWithInternalServerError();
+    changeStartValueInternalServerErrorReply();
   }
 
-  private long getNumberAsLong() throws MalformedURLException {
+  private long getSequenceNumber() throws MalformedURLException {
     return new Long(getData(VOUCHER_NUMBER_ENDPOINT)
       .then()
         .statusCode(HttpStatus.HTTP_OK.toInt())
@@ -70,7 +69,7 @@ public class VoucherNumberTest extends TestBase {
             .path(SEQUENCE_NUMBER));
   }
 
-  private static void testProcessingErrorReply() throws MalformedURLException {
+  private void getSequenceNumberWithInternalServerError() throws MalformedURLException {
     given()
       .header(TENANT_HEADER)
       .contentType(ContentType.JSON)
@@ -78,11 +77,40 @@ public class VoucherNumberTest extends TestBase {
           .then()
             .statusCode(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
             .contentType(TEXT_PLAIN)
-            .extract()
-              .response();
+              .extract()
+                .response();
   }
 
-  private static void dropSequenceInDb() throws Exception {
+  private void changeStartValue(long value) throws MalformedURLException {
+    given()
+      .header(TENANT_HEADER)
+      .contentType(ContentType.TEXT)
+        .post(storageUrl(VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT + value))
+          .then()
+            .statusCode(HttpStatus.HTTP_NO_CONTENT.toInt());
+  }
+
+  private void changeStartValueWithBadRequest() throws MalformedURLException {
+    given()
+      .header(TENANT_HEADER)
+      .contentType(ContentType.TEXT)
+        .post(storageUrl(VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT + "100.1"))
+          .then()
+            .statusCode(HttpStatus.HTTP_BAD_REQUEST.toInt())
+            .contentType(TEXT_PLAIN);
+  }
+
+  private void changeStartValueInternalServerErrorReply() throws MalformedURLException {
+    given()
+      .header(TENANT_HEADER)
+      .contentType(ContentType.TEXT)
+        .post(storageUrl(VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT + "100"))
+          .then()
+            .statusCode(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
+            .contentType(TEXT_PLAIN);
+  }
+
+  private void dropSequenceInDb() throws Exception {
     CompletableFuture<UpdateResult> future = new CompletableFuture<>();
       PostgresClient.getInstance(Vertx.vertx()).execute(DROP_SEQUENCE_QUERY, result -> {
         if(result.failed()) {
