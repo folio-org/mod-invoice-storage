da380a1ab79bbaf0bbe37f9cbb1d273a5deee075
javalkar-varun <vjavalkar@ebsco.com>
2019-05-23 11:01:08 -0400

    After merging with master

--- src/main/java/org/folio/rest/impl/VoucherNumberImpl.java
+++ src/main/java/org/folio/rest/impl/VoucherNumberImpl.java
@@ -5,45 +5,85 @@ import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-
+import org.apache.commons.lang.math.NumberUtils;
 import org.folio.rest.RestVerticle;
+import org.folio.rest.jaxrs.model.SequenceNumber;
 import org.folio.rest.jaxrs.resource.VoucherStorageVoucherNumber;
 import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.messages.MessageConsts;
 import org.folio.rest.tools.messages.Messages;
 import org.folio.rest.tools.utils.TenantTool;
 
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
+import static io.vertx.core.Future.succeededFuture;
+
 
 public class VoucherNumberImpl implements VoucherStorageVoucherNumber {
 
-  private static final Logger log = LoggerFactory.getLogger(VoucherNumberImpl.class);
   private final Messages messages = Messages.getInstance();
 
+  private static final Logger log = LoggerFactory.getLogger(VoucherNumberImpl.class);
   private static final String VOUCHER_NUMBER_QUERY = "SELECT nextval('voucher_number')";
+  private static final String SET_START_SEQUENCE_VALUE_QUERY = "ALTER SEQUENCE voucher_number START WITH %s RESTART;";
   public static final String CURRENT_VOUCHER_NUMBER_QUERY = "SELECT pg_sequences.start_value FROM pg_sequences WHERE sequencename = 'voucher_number'";
-  
+
   @Override
-  public void getVoucherStorageVoucherNumber(String lang, Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    VoucherNumberHelper getVoucherNumberHelper = new VoucherNumberHelper(okapiHeaders);
-    String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
-    PostgresClient.getInstance(vertxContext.owner(), tenantId)
-      .selectSingle(VOUCHER_NUMBER_QUERY,
-          reply -> getVoucherNumberHelper.retrieveVoucherNumber(reply, asyncResultHandler, messages, lang));
+  public void getVoucherStorageVoucherNumber(String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext((Void v) -> {
+      String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+      PostgresClient.getInstance(vertxContext.owner(), tenantId).selectSingle(VOUCHER_NUMBER_QUERY, reply -> {
+        try {
+          if(reply.succeeded()) {
+            String voucherNumber = reply.result().getList().get(0).toString();
+            log.debug("Retrieved voucher number: {}", voucherNumber);
+            SequenceNumber sequenceNumber = new SequenceNumber().withSequenceNumber(voucherNumber);
+            asyncResultHandler.handle(succeededFuture(VoucherStorageVoucherNumber.GetVoucherStorageVoucherNumberResponse.respond200WithApplicationJson(sequenceNumber)));
+          } else {
+            throw new Exception(reply.cause());
+          }
+        } catch (Exception e) {
+          log.error(e.getMessage(), e);
+          String msg = messages.getMessage(lang, MessageConsts.InternalServerError);
+          asyncResultHandler.handle(succeededFuture(VoucherStorageVoucherNumber.GetVoucherStorageVoucherNumberResponse.respond500WithTextPlain(msg)));
+        }
+      });
+    });
   }
 
   @Override
   public void getVoucherStorageVoucherNumberStart(String lang, Map<String, String> okapiHeaders,
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    log.debug(" === Retrieving current start value for a voucher number sequence === ");
-    VoucherNumberHelper getVoucherNumberStartHelper = new VoucherNumberHelper(okapiHeaders);
-    String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
-    PostgresClient.getInstance(vertxContext.owner(), tenantId)
-      .selectSingle(CURRENT_VOUCHER_NUMBER_QUERY,
-          reply -> {
-            getVoucherNumberStartHelper.retrieveVoucherNumber(reply, asyncResultHandler, messages, lang);
-          });
+//    log.debug(" === Retrieving current start value for a voucher number sequence === ");
+//    VoucherNumberHelper getVoucherNumberStartHelper = new VoucherNumberHelper(okapiHeaders);
+//    String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+//    PostgresClient.getInstance(vertxContext.owner(), tenantId)
+//      .selectSingle(CURRENT_VOUCHER_NUMBER_QUERY,
+//          reply -> {
+//            getVoucherNumberStartHelper.retrieveVoucherNumber(reply, asyncResultHandler, messages, lang);
+//          });
+  }
+
+  @Override
+  public void postVoucherStorageVoucherNumberStartByValue(String value, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext((Void v) -> {
+      if(NumberUtils.isDigits(value)) {
+        String tenantId = TenantTool.calculateTenantId(okapiHeaders.get(RestVerticle.OKAPI_HEADER_TENANT));
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).execute(String.format(SET_START_SEQUENCE_VALUE_QUERY, value), reply -> {
+          if (reply.succeeded()) {
+            log.debug("(Re)set start value for voucher number sequence: {}", value);
+            asyncResultHandler.handle(succeededFuture(VoucherStorageVoucherNumber.PostVoucherStorageVoucherNumberStartByValueResponse.respond204()));
+          } else {
+            log.error(reply.cause().getMessage(), reply.cause());
+            String msg = messages.getMessage(lang, MessageConsts.InternalServerError);
+            asyncResultHandler.handle(succeededFuture(VoucherStorageVoucherNumber.PostVoucherStorageVoucherNumberStartByValueResponse.respond500WithTextPlain(msg)));
+          }
+        });
+      } else {
+        log.debug("Illegal value: {}", value);
+        asyncResultHandler.handle(succeededFuture(VoucherStorageVoucherNumber.PostVoucherStorageVoucherNumberStartByValueResponse.respond400WithTextPlain("Bad request - illegal value")));
+      }
+    });
   }
 }
--- src/test/java/org/folio/rest/impl/VoucherNumberTest.java
+++ src/test/java/org/folio/rest/impl/VoucherNumberTest.java
@@ -1,15 +1,11 @@
 package org.folio.rest.impl;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import io.vertx.core.Vertx;
 import io.vertx.ext.sql.UpdateResult;
-import io.vertx.ext.unit.junit.Repeat;
-import io.vertx.ext.unit.junit.RepeatRule;
 import org.folio.HttpStatus;
 import org.folio.rest.persist.PostgresClient;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
@@ -19,115 +15,90 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.folio.rest.impl.StorageTestSuite.storageUrl;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class VoucherNumberTest extends TestBase {
 
-  @Rule
-  public RepeatRule rule = new RepeatRule();
-  private static List<Long> voucherNumberList;
+  private static final String VALUE = "value";
+  private static List<Long> voucherNumberList = new ArrayList<>();
 
   private static final String SEQUENCE_NUMBER = "sequenceNumber";
   private static final String VOUCHER_NUMBER_ENDPOINT = "/voucher-storage/voucher-number";
-  private static final String VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT = "/voucher-storage/voucher-number/start";
-  private static final String VOUCHER_NUMBER_INVALID_START_ENDPOINT = "/voucher-storage/voucher-number/bad_start";
+  private static final String VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT = "/voucher-storage/voucher-number/start/";
   private static final String DROP_SEQUENCE_QUERY = "DROP SEQUENCE diku_mod_invoice_storage.voucher_number";
-  private static final String RESET_START_SEQUENCE_QUERY = "ALTER SEQUENCE diku_mod_invoice_storage.voucher_number START WITH 105";
-  
-
-  @BeforeClass
-  public static void setUp() {
-    voucherNumberList  = new ArrayList<>();
-  }
+  private static final int NUM_OF_REQUESTS = 3;
 
   @Test
-  @Repeat(3)
-  public void testGetVoucherNumber() throws MalformedURLException {
-    voucherNumberList.add(getNumberAsLong());
-  }
+  public void testGetVoucherNumber() throws Exception {
 
-  @Test
-  public void testCurrentStartValueVoucherNumber() throws Exception {
-    // Get and verify Voucher number's start value defaults to 0
-    assertThat(getCurrentStartValueVoucherNumber(), equalTo(0L));
-  }
-
-  @Test
-  public void testResetedStartValueVoucherNumber() throws Exception {
-    // reset start value in sequence
-    resetStartSequenceInDb();
-    // verify current start value equals new reseted start value
-    assertThat(getCurrentStartValueVoucherNumber(), equalTo(105L));
-  }
-
-  @Test
-  public void testCurrentStartValueVoucherNumberInvalidUrl() throws MalformedURLException {
-    getData(VOUCHER_NUMBER_INVALID_START_ENDPOINT).then()
-      .statusCode(400);
-  }
-
-  @AfterClass
-  public static void tearDown() throws Exception {
+    // Retrieve voucher numbers
+    for(int i = 0; i < NUM_OF_REQUESTS; i++) {
+      voucherNumberList.add(getSequenceNumberValue());
+    }
 
     // Verify expected start value
     assertThat(voucherNumberList.get(0), equalTo(0L));
-    
+
     // Positive scenario - testing of number increase
     for(int i = 0; i < voucherNumberList.size(); i++) {
       assertThat(voucherNumberList.get(i) - voucherNumberList.get(0), equalTo((long) i));
     }
 
-    // Negative scenario - retrieving number from non-existed sequence
+    // Verify changing of start value
+    long start = 11111111L;
+
+    changeStartValueResponse(start)
+      .statusCode(HttpStatus.HTTP_NO_CONTENT.toInt());
+
+    assertThat(getSequenceNumberValue(), is(start));
+
+    // Negative scenario - changing start value with bad request
+    changeStartValueResponse(10.1)
+      .statusCode(HttpStatus.HTTP_BAD_REQUEST.toInt())
+      .contentType(ContentType.TEXT);
+
+    // Negative scenario - retrieving voucher number and changing start value from non-existed (deleted) sequence
     dropSequenceInDb();
-    testProcessingErrorReply();
+
+    getSequenceNumberResponse()
+      .statusCode(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
+      .contentType(ContentType.TEXT);
+
+    changeStartValueResponse(100)
+      .statusCode(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
+      .contentType(ContentType.TEXT);
   }
 
-  private long getCurrentStartValueVoucherNumber() throws MalformedURLException {
-    return new Long(getData(VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT)
-      .then()
-        .statusCode(HttpStatus.HTTP_OK.toInt())
-        .extract()
-          .response()
-            .path(SEQUENCE_NUMBER));
-  }
-
-  private long getNumberAsLong() throws MalformedURLException {
-    return new Long(getData(VOUCHER_NUMBER_ENDPOINT)
-      .then()
-        .statusCode(HttpStatus.HTTP_OK.toInt())
-        .extract()
-          .response()
-            .path(SEQUENCE_NUMBER));
-  }
-
-  private static void testProcessingErrorReply() throws MalformedURLException {
-    given()
+  private ValidatableResponse getSequenceNumberResponse() throws MalformedURLException {
+    return given()
       .header(TENANT_HEADER)
-      .contentType(ContentType.JSON)
-        .get(storageUrl(VOUCHER_NUMBER_ENDPOINT))
-          .then()
-            .statusCode(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt())
-            .contentType(TEXT_PLAIN)
-            .extract()
-              .response();
+        .when()
+          .get(storageUrl(VOUCHER_NUMBER_ENDPOINT))
+        .then();
   }
 
-  private static void resetStartSequenceInDb() throws Exception {
-    CompletableFuture<UpdateResult> future = new CompletableFuture<>();
-      PostgresClient.getInstance(Vertx.vertx()).execute(RESET_START_SEQUENCE_QUERY, result -> {
-        if(result.failed()) {
-          future.completeExceptionally(result.cause());
-        } else {
-          future.complete(result.result());
-        }
-      });
-      future.get(10, TimeUnit.SECONDS);
+  private ValidatableResponse changeStartValueResponse(Object value) throws MalformedURLException {
+    return given()
+      .header(TENANT_HEADER)
+      .pathParam(VALUE, value)
+        .when()
+          .post(storageUrl(VOUCHER_STORAGE_VOUCHER_NUMBER_START_ENDPOINT + "{value}"))
+        .then();
   }
-  
-  private static void dropSequenceInDb() throws Exception {
+
+  private long getSequenceNumberValue() throws MalformedURLException {
+    return Long.parseLong(
+      getSequenceNumberResponse()
+        .statusCode(HttpStatus.HTTP_OK.toInt())
+        .extract()
+          .response()
+            .path(SEQUENCE_NUMBER));
+  }
+
+  private void dropSequenceInDb() throws Exception {
     CompletableFuture<UpdateResult> future = new CompletableFuture<>();
       PostgresClient.getInstance(Vertx.vertx()).execute(DROP_SEQUENCE_QUERY, result -> {
         if(result.failed()) {
