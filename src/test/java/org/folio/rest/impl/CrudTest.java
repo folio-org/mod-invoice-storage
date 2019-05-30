f771046de4356d2b14a09eb9c7f250d8fa435b98
Siarhei_Hrabko <siarhei_hrabko@epam.com>
2019-05-30 12:57:36 +0300

    revert CrudTest

--- src/test/java/org/folio/rest/impl/CrudTest.java
+++ src/test/java/org/folio/rest/impl/CrudTest.java
@@ -40,7 +40,7 @@ private final Logger logger = LoggerFactory.getLogger(CrudTest.class);
   public void testPositiveCases() throws MalformedURLException {
   	String sampleId = null;
     try {
-
+    	
       logger.info(String.format("--- mod-invoice-storage %s test: Verifying database's initial state ... ", testEntity.name()));
       verifyCollectionQuantity(testEntity.getEndpoint(), 0);
 
@@ -48,27 +48,27 @@ private final Logger logger = LoggerFactory.getLogger(CrudTest.class);
       String sample = getFile(testEntity.getSampleFileName());
       Response response = postData(testEntity.getEndpoint(), sample);
       sampleId = response.then().extract().path("id");
-
+      
       logger.info(String.format("--- mod-invoice-storage %s test: Valid fields exists ... ", testEntity.name()));
       JsonObject sampleJson = convertToMatchingModelJson(sample, testEntity);
       JsonObject responseJson = JsonObject.mapFrom(response.then().extract().response().as(testEntity.getClazz()));
       testAllFieldsExists(responseJson, sampleJson);
-
+      
       logger.info(String.format("--- mod-invoice-storage %s test: Verifying only 1 invoice was created ... ", testEntity.name()));
       verifyCollectionQuantity(testEntity.getEndpoint(),1);
-
+      
       logger.info(String.format("--- mod-invoice-storage %s test: Fetching %s with ID: %s", testEntity.name(), testEntity.name(), sampleId));
       testEntitySuccessfullyFetched(testEntity.getEndpointWithId(), sampleId);
-
+      
       logger.info(String.format("--- mod-invoice-storage %s test: Editing %s with ID: %s", testEntity.name(), testEntity.name(), sampleId));
       JsonObject catJSON = new JsonObject(sample);
       catJSON.put("id", sampleId);
       catJSON.put(testEntity.getUpdatedFieldName(), testEntity.getUpdatedFieldValue());
       testEntityEdit(testEntity.getEndpointWithId(), catJSON.toString(), sampleId);
-
+      
       logger.info(String.format("--- mod-invoice-storage %s test: Fetching updated %s with ID: %s", testEntity.name(), testEntity.name(), sampleId));
-      testFetchingUpdatedEntity(sampleId, testEntity);
-
+      testFetchingUpdatedEntity(sampleId, testEntity);    
+      
 		} catch (Exception e) {
 			  logger.error(String.format("--- mod-invoice-storage-test: %s API ERROR: ", testEntity.name()), e);
 			  fail(e.getMessage());
@@ -102,7 +102,7 @@ private final Logger logger = LoggerFactory.getLogger(CrudTest.class);
     getDataById(testEntity.getEndpointWithId(), NON_EXISTED_ID).then().log().ifValidationFails()
       .statusCode(404);
   }
-
+  
   @Test
   public void testEditEntityWithNonExistedId() throws MalformedURLException {
     logger.info(String.format("--- mod-invoice-storage %s put by id test: Invalid %s: %s", testEntity.name(), testEntity.name(), NON_EXISTED_ID));
@@ -122,7 +122,7 @@ private final Logger logger = LoggerFactory.getLogger(CrudTest.class);
         .then()
           .statusCode(404);
   }
-
+  
   @Test
   public void testDeleteEntityWithNonExistedId() throws MalformedURLException {
     logger.info(String.format("--- mod-invoice-storage %s delete by id test: Invalid %s: %s", testEntity.name(), testEntity.name(), NON_EXISTED_ID));
