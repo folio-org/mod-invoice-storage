7cd00d11565af56c10cff6801b138d07fcf3d85c
javalkar-varun <vjavalkar@ebsco.com>
2019-07-16 09:41:42 -0400

    MODINVOSTO-29 - Implement basic CRUD for /voucher-storage/acquisitions-unit-assignments

--- src/test/java/org/folio/rest/utils/TenantApiTestUtil.java
+++ src/test/java/org/folio/rest/utils/TenantApiTestUtil.java
@@ -5,7 +5,6 @@ import io.restassured.http.Header;
 import io.restassured.response.ValidatableResponse;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-
 import java.net.MalformedURLException;
 
 import static io.restassured.RestAssured.given;
@@ -13,7 +12,7 @@ import static org.folio.rest.impl.StorageTestSuite.URL_TO_HEADER;
 import static org.folio.rest.impl.StorageTestSuite.storageUrl;
 
 public class TenantApiTestUtil {
-
+  
   public static final String TENANT_ENDPOINT = "/_/tenant";
   private static final Header USER_ID_HEADER = new Header("X-Okapi-User-id", "28d0fb04-d137-11e8-a8d5-f2801f1b9fd1");
 
@@ -21,17 +20,18 @@ public class TenantApiTestUtil {
 
   }
 
-	public static JsonObject prepareTenantBody(boolean isLoadSampleData, boolean isUpgrade) {
-		JsonArray parameterArray = new JsonArray();
-		parameterArray.add(new JsonObject().put("key", "loadSample").put("value", isLoadSampleData));
-		JsonObject jsonBody = new JsonObject();
-		jsonBody.put("module_to", "mod-invoice-storage-1.0.0");
-		jsonBody.put("parameters", parameterArray);
-		if (isUpgrade) {
-			jsonBody.put("module_from", "mod-invoice-storage-1.0.0");
-		}
-		return jsonBody;
-	}
+  public static JsonObject prepareTenantBody(boolean isLoadSampleData, boolean isUpgrade) {
+    JsonArray parameterArray = new JsonArray();
+    parameterArray.add(new JsonObject().put("key", "loadSample")
+      .put("value", isLoadSampleData));
+    JsonObject jsonBody = new JsonObject();
+    jsonBody.put("module_to", "mod-invoice-storage-1.0.0");
+    jsonBody.put("parameters", parameterArray);
+    if (isUpgrade) {
+      jsonBody.put("module_from", "mod-invoice-storage-1.0.0");
+    }
+    return jsonBody;
+  }
 
   public static void prepareTenant(Header tenantHeader, boolean isLoadSampleData) throws MalformedURLException {
     JsonObject jsonBody = prepareTenantBody(isLoadSampleData, false);
