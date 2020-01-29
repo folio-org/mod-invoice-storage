ddec0cc7f2d1ecb3964e6503f307d8d2a1c25ebc
craigmcnally <cmcnally@ebsco.com>
2020-01-29 21:34:07 +0000

    data migration scripts

--- src/main/resources/templates/db_scripts/data-migration/invoices_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/invoices_table.sql
@@ -1,3 +1,9 @@
 CREATE SEQUENCE IF NOT EXISTS ${myuniversity}_${mymodule}.invoice_number NO MAXVALUE START WITH 10000 CACHE 1 NO CYCLE;
 GRANT USAGE ON SEQUENCE ${myuniversity}_${mymodule}.invoice_number TO ${myuniversity}_${mymodule};
 CREATE UNIQUE INDEX IF NOT EXISTS invoices_invoice_number_unique_idx ON ${myuniversity}_${mymodule}.invoices ((jsonb->>'invoiceNumber'));
+
+UPDATE ${myuniversity}_${mymodule}.invoices
+SET
+  jsonb = jsonb || jsonb_build_object('batchGroupId', '2a2cb998-1437-41d1-88ad-01930aaeadd5')
+WHERE
+  NOT jsonb ? 'batchGroupId';
--- src/main/resources/templates/db_scripts/data-migration/vouchers_table.sql
+++ src/main/resources/templates/db_scripts/data-migration/vouchers_table.sql
@@ -1,2 +1,8 @@
 CREATE SEQUENCE IF NOT EXISTS ${myuniversity}_${mymodule}.voucher_number MINVALUE 0 NO MAXVALUE CACHE 1 NO CYCLE;
 ALTER SEQUENCE ${myuniversity}_${mymodule}.voucher_number OWNER TO ${myuniversity}_${mymodule};
+
+UPDATE ${myuniversity}_${mymodule}.vouchers
+SET
+  jsonb = jsonb || jsonb_build_object('batchGroupId', '2a2cb998-1437-41d1-88ad-01930aaeadd5')
+WHERE
+  NOT jsonb ? 'batchGroupId';
