4b0957fe6397aabf68c48089bf4f397c98b08802
Craig McNally <cmcnally@ebsco.com>
2020-03-27 09:44:08 -0400

    MODINVOSTO-61 - bootstrap batch_groups table (#64)
    * bootstrap batch_groups table
    
    * fix fromModule version

---
+++ src/main/resources/templates/db_scripts/batch_groups.sql
@@ -0,0 +1,1 @@
+INSERT INTO ${myuniversity}_${mymodule}.batch_groups (id, jsonb) VALUES ('2a2cb998-1437-41d1-88ad-01930aaeadd5', '{"id": "2a2cb998-1437-41d1-88ad-01930aaeadd5", "name": "FOLIO"}') ON CONFLICT DO NOTHING;
