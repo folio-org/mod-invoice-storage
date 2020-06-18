e508a82b26b04a6f39ae3c351b2793a9dcc42c8a
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2020-06-18 17:23:41 +0300

    MODINVOSTO-69 Migration script populate "fundDistribution.code" into old voucher lines
    - script written
    - RMB updated to 30.1.0
    - fixed version in pom and news

---
+++ src/test/resources/finance_schema.sql
@@ -0,0 +1,9 @@
+CREATE ROLE new_tenant_mod_finance_storage PASSWORD 'new_tenant' NOSUPERUSER NOCREATEDB INHERIT LOGIN;
+GRANT new_tenant_mod_finance_storage TO CURRENT_USER;
+CREATE SCHEMA new_tenant_mod_finance_storage AUTHORIZATION new_tenant_mod_finance_storage;
+
+
+CREATE TABLE IF NOT EXISTS new_tenant_mod_finance_storage.fund (
+  id UUID PRIMARY KEY,
+  jsonb JSONB NOT NULL
+);
