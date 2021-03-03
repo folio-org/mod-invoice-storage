246e2877f37974086c052aaf55917891c08232e8
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2021-03-03 15:00:07 +0300

    MODINVOSTO-75 Migration script for removing invoice number from the invoice line number
    Signed-off-by: Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>

--- src/main/resources/templates/db_scripts/data-migration/reference_number_migration.sql
+++ src/main/resources/templates/db_scripts/data-migration/reference_number_migration.sql
@@ -1,7 +1,5 @@
 UPDATE ${myuniversity}_${mymodule}.invoice_lines
-SET
-  jsonb = jsonb - 'vendorRefNo' || jsonb_build_object('referenceNumbers', json_build_array(jsonb_build_object(
-        'refNumber', jsonb->>'vendorRefNo',
+SET jsonb = jsonb - 'vendorRefNo' || jsonb_build_object('referenceNumbers', json_build_array(jsonb_build_object(
+        'refNumber', jsonb ->> 'vendorRefNo',
         'vendorDetailsSource', 'InvoiceLine')))
-WHERE
-  jsonb ? 'vendorRefNo';
+WHERE jsonb ? 'vendorRefNo';
