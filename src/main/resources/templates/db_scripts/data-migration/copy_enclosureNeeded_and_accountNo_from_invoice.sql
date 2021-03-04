036a520b5b2542ea9fb314ceed25759fdb5f1204
Damien <damien-git@users.noreply.github.com>
2021-03-04 12:50:36 -0500

    [MODINVOSTO-106] Fixed migration script

--- src/main/resources/templates/db_scripts/data-migration/copy_enclosureNeeded_and_accountNo_from_invoice.sql
+++ src/main/resources/templates/db_scripts/data-migration/copy_enclosureNeeded_and_accountNo_from_invoice.sql
@@ -1,6 +1,6 @@
 -- Copy enclosureNeeded and accountNo from the invoice
 UPDATE ${myuniversity}_${mymodule}.vouchers v
 SET jsonb = jsonb ||
-  jsonb_strip_nulls(jsonb_build_object('enclosureNeeded', i.enclosureNeeded, 'accountNo', i.accountNo))
+  jsonb_strip_nulls(jsonb_build_object('enclosureNeeded', i.jsonb->'enclosureNeeded', 'accountNo', i.jsonb->'accountNo'))
 FROM ${myuniversity}_${mymodule}.invoices i
 WHERE v.invoiceID = i.id;
