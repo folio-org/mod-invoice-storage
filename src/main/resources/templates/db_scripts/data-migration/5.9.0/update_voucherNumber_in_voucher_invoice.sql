236819e6cc15b6ee68d251593d94fdfa25101829
yuntianhu <48864579+yuntianhu@users.noreply.github.com>
2024-04-11 10:03:16 -0400

    MODINVOSTO-172 updated the schema.json and removed update the voucher table

--- src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
+++ src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
@@ -1,6 +1,3 @@
-UPDATE ${myuniversity}_${mymodule}.vouchers
-SET jsonb = jsonb || jsonb_build_object('voucherNumber', jsonb ->> 'voucherNumber');
-
 UPDATE ${myuniversity}_${mymodule}.invoices AS i
 SET jsonb = jsonb || jsonb_build_object('voucherNumber', v.jsonb ->> 'voucherNumber')
   FROM ${myuniversity}_${mymodule}.vouchers AS v
