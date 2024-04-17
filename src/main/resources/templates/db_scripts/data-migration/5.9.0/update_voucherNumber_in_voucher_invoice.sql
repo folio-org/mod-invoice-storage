6ecd90d34b0f732065ed0169666a1100fb34eccf
yuntianhu <48864579+yuntianhu@users.noreply.github.com>
2024-04-17 14:20:45 -0400

    correct the schema.json jsonb ambiguous fix

--- src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
+++ src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
@@ -1,4 +1,4 @@
 UPDATE ${myuniversity}_${mymodule}.invoices AS i
-SET jsonb = jsonb || jsonb_build_object('voucherNumber', v.jsonb ->> 'voucherNumber')
+SET jsonb = i.jsonb || jsonb_build_object('voucherNumber', v.jsonb ->> 'voucherNumber')
   FROM ${myuniversity}_${mymodule}.vouchers AS v
 WHERE i.id = v.invoiceId;
