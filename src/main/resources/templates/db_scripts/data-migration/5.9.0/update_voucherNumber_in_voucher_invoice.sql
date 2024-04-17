16e2238514a2dfa20d4fdd664682c28d4bd324d8
yuntianhu <48864579+yuntianhu@users.noreply.github.com>
2024-04-17 12:31:03 -0400

    correct the schema.json invoice id

--- src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
+++ src/main/resources/templates/db_scripts/data-migration/5.9.0/update_voucherNumber_in_voucher_invoice.sql
@@ -1,4 +1,4 @@
 UPDATE ${myuniversity}_${mymodule}.invoices AS i
 SET jsonb = jsonb || jsonb_build_object('voucherNumber', v.jsonb ->> 'voucherNumber')
   FROM ${myuniversity}_${mymodule}.vouchers AS v
-WHERE i.invoiceId = v.invoiceId;
+WHERE i.id = v.invoiceId;
