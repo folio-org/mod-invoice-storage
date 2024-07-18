72a042f5aa3c079e091e6df1d9b319e46706fc4a
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-07-18 11:29:08 +0500

    [MODINV-PTF-OPT]. Add extra search and sorting indexes (#188)

---
+++ src/main/resources/templates/db_scripts/invoices_table.sql
@@ -0,0 +1,14 @@
+CREATE INDEX IF NOT EXISTS invoices_invoice_date_sort ON ${myuniversity}_${mymodule}.invoices
+  (left(lower(f_unaccent(invoices.jsonb->>'invoiceDate')),600), lower(f_unaccent(invoices.jsonb->>'invoiceDate')));
+
+CREATE INDEX IF NOT EXISTS invoice_num_sort ON ${myuniversity}_${mymodule}.invoices
+  (left(lower(f_unaccent(invoices.jsonb->>'vendorInvoiceNo')),600), lower(f_unaccent(invoices.jsonb->>'vendorInvoiceNo')));
+
+CREATE INDEX IF NOT EXISTS invoices_status_sort ON ${myuniversity}_${mymodule}.invoices
+  (left(lower(f_unaccent(invoices.jsonb->>'status')),600), lower(f_unaccent(invoices.jsonb->>'status')));
+
+CREATE INDEX IF NOT EXISTS invoices_invoice_total_sort ON ${myuniversity}_${mymodule}.invoices
+  (left(lower(f_unaccent(invoices.jsonb->>'invoiceTotal')),600), lower(f_unaccent(invoices.jsonb->>'invoiceTotal')));
+
+CREATE INDEX IF NOT EXISTS invoices_no_acq_unit ON ${myuniversity}_${mymodule}.invoices(jsonb)
+  WHERE left(lower(f_unaccent(jsonb ->> 'acqUnitIds')), 600) NOT LIKE '[]';
