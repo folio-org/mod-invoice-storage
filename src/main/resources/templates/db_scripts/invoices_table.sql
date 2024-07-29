ac622bafc9cef29ec4e6614b3a840c9a1403a954
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-07-29 11:10:07 +0500

    [MODINVOSTO-IDX-FIX]. Remove faulty invoices_no_acq_unit index

--- src/main/resources/templates/db_scripts/invoices_table.sql
+++ src/main/resources/templates/db_scripts/invoices_table.sql
@@ -9,6 +9,3 @@ CREATE INDEX IF NOT EXISTS invoices_status_sort ON ${myuniversity}_${mymodule}.i
 
 CREATE INDEX IF NOT EXISTS invoices_invoice_total_sort ON ${myuniversity}_${mymodule}.invoices
   (left(lower(f_unaccent(invoices.jsonb->>'invoiceTotal')),600), lower(f_unaccent(invoices.jsonb->>'invoiceTotal')));
-
-CREATE INDEX IF NOT EXISTS invoices_no_acq_unit ON ${myuniversity}_${mymodule}.invoices(jsonb)
-  WHERE left(lower(f_unaccent(jsonb ->> 'acqUnitIds')), 600) NOT LIKE '[]';
